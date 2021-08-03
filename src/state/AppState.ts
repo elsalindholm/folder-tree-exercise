1bd5040fdb708ea85258b16671ab22e33f1182be
elsalindholm <31487907+elsalindholm@users.noreply.github.com>
2021-08-03 10:25:59 +0100

    Root Folders can be created

--- src/state/AppState.ts
+++ src/state/AppState.ts
@@ -9,19 +9,38 @@ export enum NodeType {
 
 export class AppState {
   public treeRoot = new Folder('', '', '');
-  @observable public treeNodes = new Map<string, TreeNode>();
+  @observable public nodeMap = new Map<string, TreeNode>();
 
   constructor() {
-    this.treeNodes.set(this.treeRoot.id, this.treeRoot);
+    this.nodeMap.set(this.treeRoot.id, this.treeRoot);
   }
 
   @action createRootFolder() {
     const parentId = ''; // root folder parents have no id
-    const newRootFolder = new Folder('new', parentId, 'New Folder');
+    let folderId = this.createRandomId();
+    const newRootFolder = new Folder(folderId, parentId, 'New Folder');
 
-    const parent = this.treeNodes.get(parentId);
+    const parent = this.nodeMap.get(parentId) as Folder;
 
-    this.treeNodes.set(newRootFolder.id, newRootFolder);
-    console.log(this.treeNodes);
+    if (parent) {
+      parent.children.push(newRootFolder);
+      console.log(`${parent} is a parent of ${newRootFolder}`);
+    }
+
+    this.nodeMap.set(newRootFolder.id, newRootFolder);
+    console.log(this.nodeMap);
+  }
+
+  @action createRandomId() {
+    let charsForId = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    let lengthOfId = 6;
+    let randomId: string[] = [];
+
+    for (let i = 0; i < lengthOfId; i++) {
+      let index = Math.floor(Math.random() * (charsForId.length - 1));
+      randomId.push(charsForId[index]);
+    }
+    console.log(randomId);
+    return randomId.join('');
   }
 }
